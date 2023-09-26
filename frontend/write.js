2f7dce05ad80d4c28b39d104eb54ac2deb80e2f9
unchul <105141025+unchul@users.noreply.github.com>
2023-09-26 19:36:39 +0900

    서버 연동
    완료

---
+++ frontend/write.js
@@ -0,0 +1,18 @@
+const form = document.getElementById("write-form");
+
+const handlSubmitForm = async (event) => {
+  event.preventDefault();
+  const body = new FormData(form);
+  body.append("insertAt", new Date().getTime());
+  try {
+    const res = await fetch("/items", {
+      method: "POST",
+      body,
+    });
+    const data = await res.json();
+    if (data === "200") window.location.pathname = "/";
+  } catch (e) {
+    console.error(e);
+  }
+};
+form.addEventListener("submit", handlSubmitForm);
